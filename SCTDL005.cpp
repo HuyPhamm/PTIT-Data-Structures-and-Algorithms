d1a80340d0005c4b271491ddb883f01275a67725
Phạm Hoàng Huy <87490676+HuyPhamm@users.noreply.github.com>
2023-02-08 23:51:29 +0700

    Add files via upload

---
+++ SCTDL005.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[100];
+
+int main()
+{
+	int t;cin>>t;
+	while(t--)
+	{
+		int n;cin>>n;
+		for(int i=0;i<n;i++)
+			a[i]=i+1;
+		do
+		{
+			for(int i=0;i<n;i++)
+				cout<<a[i];
+			cout<<" ";
+		}
+		while(next_permutation(a,a+n));
+		cout<<endl;
+	}
+}
